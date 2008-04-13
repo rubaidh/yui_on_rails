1bfcee63ff4b717470b6d47137b0afc54cea529e
Graeme Mathieson <mathie@woss.name>
2008-04-13 11:15:00 +0100

    Render a Yahoo calendar control.
    First attempt at getting a Yahoo calendar control rendered in the form instead
    of a regular date select. The date select fields are still rendered (hidden)
    to allow the calendar to communicate back with the Rails app.

--- lib/action_view/helpers/yui/calendar_helper.rb
+++ lib/action_view/helpers/yui/calendar_helper.rb
@@ -9,9 +9,87 @@ module ActionView
     end
 
     class InstanceTag
+      include Helpers::JavaScriptHelper
+      include Helpers::FormHelper
+
       def to_calendar_select_tag(options = {}, html_options = {})
-        "Hello World."
+        defaults = { :discard_type => true }
+        options = defaults.merge(options)
+
+        datetime = value(object)
+
+        calendar_select_tag = select_year(datetime, options_with_prefix(1, options.merge(:use_hidden => true)), html_options)
+        calendar_select_tag += select_month(datetime, options_with_prefix(2, options.merge(:use_hidden => true)), html_options)
+        calendar_select_tag += select_day(datetime, options_with_prefix(3, options.merge(:use_hidden => true)), html_options)
+
+        add_default_name_and_id(options)
+
+        calendar_select_tag += content_tag(:div, '', :id => options["id"])
+
+        namespace = "Rails.Calendar.#{object_name.camelize}.#{method_name.camelize}"
+        calendar_select_tag += javascript_tag <<-JS
+          YAHOO.namespace('#{namespace}');
+          YAHOO.#{namespace}.initializeCalendar = function() {
+
+            var day        = YAHOO.util.Dom.get("#{day_dom_id(options)}").value;
+            var month      = YAHOO.util.Dom.get("#{month_dom_id(options)}").value;
+            var year       = YAHOO.util.Dom.get("#{year_dom_id(options)}").value;
+            var dateString = month + "/" + day + "/" + year;
+
+            var theCalendar = new YAHOO.widget.Calendar('#{options["id"]}', {
+              navigator: true,
+              start_weekday: 1,
+              selected: dateString
+            });
+
+            theCalendar.render();
+
+            theCalendar.selectEvent.subscribe(function(type, args) {
+              var dates = args[0];
+              var date = dates[0];
+
+              var dayField   = YAHOO.util.Dom.get("#{day_dom_id(options)}");
+              var monthField = YAHOO.util.Dom.get("#{month_dom_id(options)}");
+              var yearField  = YAHOO.util.Dom.get("#{year_dom_id(options)}");
+
+              var day   = date[2];
+              var month = date[1];
+              var year  = date[0];
+
+              dayField.value   = day;
+              monthField.value = month;
+              yearField.value  = year;
+            });
+          }
+
+          YAHOO.util.Event.onDOMReady(YAHOO.#{namespace}.initializeCalendar);
+        JS
+
+        calendar_select_tag
       end
+
+      private
+      def year_dom_id(options = {})
+        # FIXME: Magic numbers from #date_or_time_select's position hash.
+        options = options_with_prefix(1, options)
+        name_and_id_from_options(options, 'year')
+        options[:id]
+      end
+
+      def month_dom_id(options = {})
+        # FIXME: Magic numbers from #date_or_time_select's position hash.
+        options = options_with_prefix(2, options)
+        name_and_id_from_options(options, 'month')
+        options[:id]
+      end
+
+      def day_dom_id(options = {})
+        # FIXME: Magic numbers from #date_or_time_select's position hash.
+        options = options_with_prefix(3, options)
+        name_and_id_from_options(options, 'day')
+        options[:id]
+      end
+
     end
 
     class FormBuilder
