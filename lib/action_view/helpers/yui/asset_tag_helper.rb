a07c30f7347c8d3305c65de11561cde0653b158b
Graeme Mathieson <mathie@woss.name>
2008-04-14 18:55:56 +0100

    Take note of all the component dependencies.

--- lib/action_view/helpers/yui/asset_tag_helper.rb
+++ lib/action_view/helpers/yui/asset_tag_helper.rb
@@ -18,7 +18,49 @@ module ActionView # :nodoc:
         EXPERIMENTAL_JS_COMPONENTS = ["charts", "uploader"].freeze
         JS_COMPONENTS = (STABLE_JS_COMPONENTS + BETA_JS_COMPONENTS + EXPERIMENTAL_JS_COMPONENTS).sort.freeze
 
+        # All the dependencies of each YUI component.  This is cribbed from
+        # playing around with:
+        #
+        #   http://developer.yahoo.com/yui/articles/hosting/#configure
         JS_COMPONENT_DEPENDENCIES = {
+          "animation"           => ["yahoo", "dom", "event"],
+          "autocomplete"        => ["yahoo", "dom", "event"],
+          "button"              => ["yahoo", "dom", "event", "element"],
+          "calendar"            => ["yahoo", "dom", "event"],
+          "charts"              => ["yahoo", "dom", "event", "element", "json", "datasource"],
+          "colorpicker"         => ["yahoo", "dom", "event", "dragdrop", "slider", "element"],
+          "connection"          => ["yahoo", "event", "connection"],
+          "container"           => ["yahoo", "dom", "event"],
+          "cookie"              => ["yahoo"],
+          "datasource"          => ["yahoo", "event"],
+          "datatable"           => ["yahoo", "dom", "event", "element", "datasource"],
+          "dom"                 => ["yahoo"],
+          "dragdrop"            => ["yahoo", "dom", "event"],
+          "editor"              => ["yahoo", "dom", "event", "container", "menu", "element", "button"],
+          "element"             => ["yahoo", "dom", "event"],
+          "event"               => ["yahoo"],
+          "get"                 => ["yahoo"],
+          "history"             => ["yahoo", "event"],
+          "imagecropper"        => ["yahoo", "dom", "event", "dragdrop", "element", "resize"],
+          "imageloader"         => ["yahoo", "dom", "event"],
+          "json"                => ["yahoo"],
+          "layout"              => ["yahoo", "dom", "event", "element"],
+          "logger"              => ["yahoo", "dom", "event"],
+          "menu"                => ["yahoo", "dom", "event", "container"],
+          "profiler"            => ["yahoo"],
+          "profilerviewer"      => ["yuiloader", "profiler", "dom", "event", "element"],
+          "resize"              => ["yahoo", "dom", "event", "dragdrop", "element"],
+          "selector"            => ["yahoo", "dom"],
+          "slider"              => ["yahoo", "dom", "event", "dragdrop"],
+          "tabview"             => ["yahoo", "dom", "event", "element"],
+          "treeview"            => ["yahoo", "event"],
+          "uploader"            => ["yahoo"],
+          "utilities"           => [],
+          "yahoo"               => [],
+          "yahoo-dom-event"     => [],
+          "yuiloader"           => [],
+          "yuiloader-dom-event" => [],
+          "yuitest"             => ["yahoo", "dom", "event", "logger", "yuitest"]
         }
 
         CSS_COMPONENTS = [ "base", "fonts", "grids", "reset", "reset-fonts", "reset-fonts-grids"]
